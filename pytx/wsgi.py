a313c6f0ba2e8fa4ec6edb217bbf4caa34743077
Paul Bailey <paul.m.bailey@gmail.com>
2014-04-07 15:44:19 +0000

    fixed path

--- pytx/wsgi.py
+++ pytx/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pytx.settings")
+
+base_dir = os.path.dirname(__file__)
+ts = os.path.join(base_dir, 'TwoSpaces')
+sys.path.append(ts)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
