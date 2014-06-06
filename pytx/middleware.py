15d8ae0ad8e756e3670f70bc94da3f59da3d706c
Paul Bailey <paul.m.bailey@gmail.com>
2014-06-06 21:36:51 +0000

    updated for SSL

--- pytx/middleware.py
+++ pytx/middleware.py
@@ -4,12 +4,7 @@ from django import http
 class WWWMiddleware (object):
   def process_request (self, request):
     if not settings.DEBUG:
-      if not request.get_host().startswith('www'):
-        url = 'http'
-        if request.is_secure():
-          url += 's'
-          
-        url += '://www.pytexas.org' + request.get_full_path()
-        
+      if not request.get_host().startswith('www') or not request.is_secure():
+        url = 'https://www.pytexas.org' + request.get_full_path()
         return http.HttpResponseRedirect(url)
         
