3769b29dace5d709544d6e06438af301717978b9
Paul Bailey <paul.m.bailey@gmail.com>
2014-06-09 20:24:52 +0000

    added fabric for deployment

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import run, sudo, cd, hosts
+
+WEB_USER = "www-data"
+
+@hosts('pytexas.org')
+def deploy ():
+  with cd('/var/www/PyTexasWeb/'):
+    sudo('git pull', user=WEB_USER)
+    sudo('git submodule update', user=WEB_USER)
+    sudo('su -c "pip install -r requirements.txt --user" {}'.format(WEB_USER))
+    sudo('su -c "./manage.py collectstatic --noinput" {}'.format(WEB_USER))
+    
+    sudo('sudo supervisorctl restart pytx')
+    
