9894e795c613aba8023d9bf705b94b6552e154da
Paul Bailey <paul.m.bailey@gmail.com>
2015-04-12 15:28:36 +0000

    added release.txt

--- backend/pytx/management/commands/build.py
+++ backend/pytx/management/commands/build.py
@@ -71,3 +71,6 @@ class Command (BaseCommand):
     gen_path = os.path.join(deploy_dir, 'generated')
     subprocess.call("date > {}".format(gen_path), shell=True)
     
+    release_path = os.path.join(deploy_dir, slug, 'release.txt')
+    subprocess.call("echo '{}' > {}".format(rel, release_path), shell=True)
+    
