8460e70c7e1c322a154e58480b2215b8289f56c0
Paul Bailey <paul.m.bailey@gmail.com>
2015-04-13 19:50:51 +0000

    talk detail

---
+++ frontend/controllers/speakers/talk-detail-ctrl.js
@@ -0,0 +1,18 @@
+pytx.controller('TalkDetailCtrl', function($scope, $routeParams, APIFactory) {
+  $scope.APIService = new APIFactory('v1');
+  $scope.set_title('Talk Details');
+  
+  $scope.load_talk = function (data) {
+    $scope.talk = data;
+  };
+  
+  $scope.get_talk = function () {
+    $scope.APIService.get('speakers/talk/' + $routeParams.id)
+      .success($scope.load_talk)
+      .error(function () {
+        $scope.show_error('Error Loading Talk');
+      });
+  };
+  
+  $scope.get_talk();
+});
